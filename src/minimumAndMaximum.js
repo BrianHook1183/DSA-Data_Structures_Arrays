f8c8cd871d667411af728fe3a3f3420c9cb0899b
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-07-06 17:16:33 -0500

    submitted and annotated

--- src/minimumAndMaximum.js
+++ src/minimumAndMaximum.js
@@ -1,11 +1,69 @@
-/**
- * Implement an algorithm that sorts the array then returns the minimum and maximum
- */
-function minimumAndMaximumSort(numbers) {}
+/* 
+* Find maximum and minimum
+? Given an unsorted array of numbers, find the maximum and minimum numbers in the array. For example, given the array [8, 3, 7, 9, 4, 1, 2, 5, 6] your function should return an output array with two values like this: [1, 9], where the first element is the minimum and the second element is the maximum. If the given array is empty return the empty array [].
 
-/**
+! Keep in mind that numbers in the array may also be negative.
+
+TODO Implement two different algorithms to solve this problem.
+/*
+
+/*
+ * an algorithm that sorts the array then returns the minimum and maximum
+ */
+// TODO Start by sorting the array using the built-in array sort() method then simply return the first and last elements of the array.
+//! .sort() only works for this problem because it is assumed that the input values will not be greater than a single digit (-9 through 9). Note that sort() compares the string values of array elements, so the results will not be in numerical order. E.g. [5,0,-3,510000,6].sort() outputs [ -3, 0, 5, 510000, 6 ]. The sort() method will work to pass the test cases, since the numbers in the tests range within -9 and 9.
+
+//* Pseudocode from Qualified Quiz was:
+/* 
+sort the numbers array
+return [numbers[0], numbers[numbers.length - 1]]
+*/
+
+//* O(n log n) version
+function minimumAndMaximumSort(numbers) {
+  const minMax = [];
+  if (numbers.length >= 1) {
+    numbers.sort((a, b) => a - b);
+
+    minMax.push(numbers[0]);
+    minMax.push(numbers[numbers.length - 1]);
+  }
+  return minMax;
+}
+
+/*
  * Implement an algorithm that uses iteration to find the minimum and maximum
  */
-function minimumAndMaximumIterate(numbers) {}
+// Iterate over the entire array and track the smallest (minimum) and largest (maximum) numbers found so far.
+
+//* Pseudocode from Qualified Quiz was:
+/* 
+largest= negative infinity
+smallest = positive infinity
+for each e in numbers do
+  if e is larger than largest
+    largest = e
+  if e is smaller than smallest
+    smallest = e
+return [smallest, largest]
+*/
+
+//* O(n) version
+function minimumAndMaximumIterate(numbers) {
+  if (numbers.length >= 1) {
+    let min = numbers[0];
+    let max = numbers[0];
+    numbers.forEach((number) => {
+      if (number < min) {
+        min = number;
+      }
+      if (number > max) {
+        max = number;
+      }
+    });
+    return [min, max];
+  }
+  return [];
+}
 
 module.exports = { minimumAndMaximumIterate, minimumAndMaximumSort };
--- src/missingNumber.js
+++ src/missingNumber.js
@@ -1,6 +1,15 @@
 /**
  * Implement a brute force algorithm for finding the missing number in an array
  */
+//TODO  Use a loop to consider every possible integer from 1 to n and check if that integer exists in the array or not.
+//*  Pseudocode from Qualified Quiz was:
+/* 
+for i = 1 to n
+  if n is not in numbers
+    return i
+ */
+
+//* O(n^2) version
 function missingNumberBruteForce(numbers) {
   for (let i = 0; i < numbers.length; i++) {
     let match = true;
@@ -12,40 +21,54 @@ function missingNumberBruteForce(numbers) {
     }
   }
 }
-/* 
-var missing = [];
-var a = [5];
-var count = 5;
-var found = false;
-
-for (var j = 1; j < count; j++) {
-  found = false;
-  for (var i = 0; i <= a.length; i++) {
-    if (a[i] == j) {
-      found = true;
-      break;
-    }
-  }
-  if (!found) {
-    missing.push(j);
-  }
-}
-alert(JSON.stringify(missing));
- */
 
 /**
- * Use an iterative  strategy for finding the missing number in an array
+ * Use an iterative strategy for finding the missing number in an array
  */
-
+//* A program is called recursive when an entity calls itself. A program is call iterative when there is a loop (or repetition).
 //TODO Find the expected sum of all integers from 1 to n. Then find the actual sum of the numbers in the array. The difference is the missing number.
+//* Pseudocode from Qualified Quiz was:
+/* 
+expectSum = find the sum of all integers from 1 to n
+actualSum = find the sum of all numbers in the array
+return expectedSum - actualSum
+*/
+
+//* O(n) version
 function missingNumberSum(numbers) {
-  const n = numbers.length;
-  let total = ((n + 2) * (n + 1)) / 2;
-  // let total = (n*(n+1)/2);
-  numbers.map((number) => {
+  const n = numbers.length + 1;
+  let total = (n * (n + 1)) / 2;
+
+  numbers.forEach((number) => {
     total -= number;
   });
   return total;
 }
+
+//* more literal version that follows prompt's pseudocode:
+/* 
+function missingNumberSum(numbers) {
+  const n = numbers.length + 1;
+  let expectedSum = (n * (n + 1)) / 2;
+  let actualSum = 0;
+
+  numbers.forEach((number) => {
+    actualSum += number;
+  });
+  return expectedSum - actualSum;
+}
+*/
+
+// Specifically asks not to use recursion but this is a past example of finding the sum:
+/* 
+function sum(n) {
+  // Check the base case
+  if (n === 1) {
+    return 1;
+  }
+
+  return n + sum(n - 1);
+}
+ */
 
 module.exports = { missingNumberBruteForce, missingNumberSum };
